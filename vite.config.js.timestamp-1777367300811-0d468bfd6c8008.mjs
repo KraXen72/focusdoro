c97082269cb947e114c329d8b95338256b8f6deb
KraXen72 <dpelech1@gmail.com>
2026-05-18 15:28:35 +0200

    pnpm v11

---
+++ vite.config.js.timestamp-1777367300811-0d468bfd6c8008.mjs
@@ -0,0 +1,13 @@
+// vite.config.js
+import { sveltekit } from "file:///home/nex/coding/svelte/focusdoro/node_modules/.pnpm/@sveltejs+kit@2.58.0_@sveltejs+vite-plugin-svelte@4.0.4_svelte@5.55.5_vite@5.4.21_@type_4adae9b1554e16f957399b9444f395df/node_modules/@sveltejs/kit/src/exports/vite/index.js";
+import { defineConfig } from "file:///home/nex/coding/svelte/focusdoro/node_modules/.pnpm/vite@5.4.21_@types+node@25.6.0/node_modules/vite/dist/node/index.js";
+var vite_config_default = defineConfig({
+  plugins: [sveltekit()]
+  // build: {
+  // 	target: 'esnext'
+  // },
+});
+export {
+  vite_config_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsidml0ZS5jb25maWcuanMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbImNvbnN0IF9fdml0ZV9pbmplY3RlZF9vcmlnaW5hbF9kaXJuYW1lID0gXCIvaG9tZS9uZXgvY29kaW5nL3N2ZWx0ZS9mb2N1c2Rvcm9cIjtjb25zdCBfX3ZpdGVfaW5qZWN0ZWRfb3JpZ2luYWxfZmlsZW5hbWUgPSBcIi9ob21lL25leC9jb2Rpbmcvc3ZlbHRlL2ZvY3VzZG9yby92aXRlLmNvbmZpZy5qc1wiO2NvbnN0IF9fdml0ZV9pbmplY3RlZF9vcmlnaW5hbF9pbXBvcnRfbWV0YV91cmwgPSBcImZpbGU6Ly8vaG9tZS9uZXgvY29kaW5nL3N2ZWx0ZS9mb2N1c2Rvcm8vdml0ZS5jb25maWcuanNcIjtpbXBvcnQgeyBzdmVsdGVraXQgfSBmcm9tICdAc3ZlbHRlanMva2l0L3ZpdGUnO1xuaW1wb3J0IHsgZGVmaW5lQ29uZmlnIH0gZnJvbSAndml0ZSc7XG5cbmV4cG9ydCBkZWZhdWx0IGRlZmluZUNvbmZpZyh7XG5cdHBsdWdpbnM6IFtzdmVsdGVraXQoKV1cblx0Ly8gYnVpbGQ6IHtcblx0Ly8gXHR0YXJnZXQ6ICdlc25leHQnXG5cdC8vIH0sXG59KTtcbiJdLAogICJtYXBwaW5ncyI6ICI7QUFBcVIsU0FBUyxpQkFBaUI7QUFDL1MsU0FBUyxvQkFBb0I7QUFFN0IsSUFBTyxzQkFBUSxhQUFhO0FBQUEsRUFDM0IsU0FBUyxDQUFDLFVBQVUsQ0FBQztBQUFBO0FBQUE7QUFBQTtBQUl0QixDQUFDOyIsCiAgIm5hbWVzIjogW10KfQo=
